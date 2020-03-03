a8133c3a46f6822a39344d4b3fd620d7a0945869
40285570 <thomasheaton97@gmail.com>
2020-03-03 18:34:58 +0000

    6. Fix to setup_activity. Implemented switches.

--- app/src/main/java/com/example/budgeter_v01/setup_activity.java
+++ app/src/main/java/com/example/budgeter_v01/setup_activity.java
@@ -3,7 +3,9 @@ package com.example.budgeter_v01;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Button;
 import android.content.Intent;
@@ -14,7 +16,7 @@ public class setup_activity extends AppCompatActivity {
     public static int monthlySaving;
     public static Boolean busTravel;
     public static Boolean carTravel;
-    public static Boolean notAvailable;
+    public static Boolean notApplicable;
 
 
     @Override
@@ -28,26 +30,26 @@ public class setup_activity extends AppCompatActivity {
         final TextView mnthlyTxtView = (TextView) findViewById(R.id.monthlyTxtView);
         final TextView savingTxtView = (TextView) findViewById(R.id.savingTxtView);
         final Button continueButton = (Button) findViewById(R.id.Btn_Continue);
+        final Switch carTravelSwitch = (Switch) findViewById(R.id.switchCar);
+        final Switch busTravelSwitch = (Switch) findViewById(R.id.switchBus);
+        final Switch NATravelSwitch = (Switch) findViewById(R.id.switchNA);
 
-
+        // SEEK BAR LISTENERS
         monthlySBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 int y = seekBar.getProgress();
                 mnthlyTxtView.setText(String.valueOf(y));
             }
-
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
 
             }
-
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 monthlyPayment = seekBar.getProgress();
             }
         });
-
 
         savingSBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -57,13 +59,52 @@ public class setup_activity extends AppCompatActivity {
             }
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) { }
-
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 monthlySaving = seekBar.getProgress();
                 continueButton.setVisibility(View.VISIBLE);
             }
         });
+
+        // SWITCH LISTENERS
+        carTravelSwitch.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(compoundButton.isChecked()) {
+                    carTravel = true;
+                    notApplicable = false;
+                    NATravelSwitch.setChecked(false);
+                } else {
+                    carTravel = false;
+                }
+            }
+        });
+        busTravelSwitch.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(compoundButton.isChecked()) {
+                    busTravel = true;
+                    notApplicable = false;
+                    NATravelSwitch.setChecked(false);
+                } else {
+                    busTravel = false;
+                }
+            }
+        });
+        NATravelSwitch.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(compoundButton.isChecked()) {
+                    notApplicable = true;
+                    busTravel = false;
+                    carTravel = false;
+
+                    busTravelSwitch.setChecked(false);
+                    carTravelSwitch.setChecked(false);
+                }
+            }
+        });
+
     }
 }
 
