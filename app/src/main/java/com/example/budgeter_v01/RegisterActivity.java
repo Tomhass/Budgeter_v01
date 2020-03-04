b268d2abef3834b077b2918b5ae04d0d3785bffc
40285570 <thomasheaton97@gmail.com>
2020-03-04 16:53:55 +0000

    7. Added firebase dependencies and maven repos

--- app/src/main/java/com/example/budgeter_v01/RegisterActivity.java
+++ app/src/main/java/com/example/budgeter_v01/RegisterActivity.java
@@ -1,10 +1,10 @@
 package com.example.budgeter_v01;
 
-        import android.content.Intent;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.Button;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/budgeter_v01/activity_setup2.java
+++ app/src/main/java/com/example/budgeter_v01/activity_setup2.java
@@ -1,6 +1,6 @@
 package com.example.budgeter_v01;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class activity_setup2 extends AppCompatActivity {
